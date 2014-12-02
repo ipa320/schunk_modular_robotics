8d9d74a7732e353c0d31afe8365dd0fadf7d4125
thiagodefreitas <thiagodefreitas@gmail.com>
2014-12-02 17:05:04 +0100

    Stops movement after some seconds to avoid overheating and damage to the finger

--- schunk_sdhx/src/finger_driver.py
+++ schunk_sdhx/src/finger_driver.py
@@ -55,25 +55,33 @@
 #
 #################################################################
 
-import roslib; roslib.load_manifest('schunk_finger')
+import roslib
+roslib.load_manifest('schunk_finger')
 
 from control_msgs.msg import *
 from sensor_msgs.msg import *
 
-FJT_ACTION_NAME="finger_controller/follow_joint_trajectory"
+FJT_ACTION_NAME = "finger/joint_trajectory_controller/follow_joint_trajectory"
 
 import serial
 import time
 import rospy
 import actionlib
+import math
 
 Terminator="\r\n"
-commands = {"position": "p", "stop": "s", "move" : "m"}
+commands = {"position": "p", "stop": "s", "move": "m", "set_pwm":"set", "get_pwm":"get"}
 
 class Finger():
 
   def __init__(self, port):
-  
+
+    port = rospy.get_param("~devicestring")
+    self.min_pwm0 = rospy.get_param("~min_pwm0")
+    self.min_pwm1 = rospy.get_param("~min_pwm1")
+    self.max_pwm0 = rospy.get_param("~max_pwm0")
+    self.max_pwm1 = rospy.get_param("~max_pwm1")
+
     self._ser = serial.Serial(port, timeout=1.2)
     
     self._as = actionlib.SimpleActionServer(FJT_ACTION_NAME, FollowJointTrajectoryAction, self.execute_cb, False)
@@ -82,16 +90,57 @@ class Finger():
     self._pub_controller_state = rospy.Publisher('finger_controller/state', JointTrajectoryControllerState,queue_size=5)
     # joint state
     self._joint_states = JointState()
-    self._joint_states.name = ["finger_distal", "finger_proximal"]
+    self._joint_states.name = rospy.get_param("~joint_names")
     self._joint_states.velocity = [0.0,0.0]
     self._joint_states.effort = [0.0,0.0]
     # controller state
     self._controller_state = JointTrajectoryControllerState()
 
+    self.set_pwm("min_pwm0")
+    self.set_pwm("min_pwm1")
+    self.set_pwm("max_pwm0")
+    self.set_pwm("max_pwm1")
+
+    self.pos = [0,0]
+
+  def set_pwm(self, pwm_to_set):
+    complement = " "+(str)(pwm_to_set)+" "+(str)(eval("self."+pwm_to_set))+Terminator
+
+    self.execute_command("set_pwm", complement)
+
+    status, reply = self.get_pwm(pwm_to_set)
+
+    if((int)(reply) ==(eval("self."+pwm_to_set))):
+      status = True
+    else:
+      status = False
+
+    return status, reply
+
+  def get_pwm(self, pwm_to_get):
+    complement = " "+(str)(pwm_to_get)+Terminator
+
+    status, reply = self.execute_command("get_pwm", complement)
+
+    while(pwm_to_get not in reply):
+      status, reply = self.execute_command("get_pwm", complement)
+
+    reply = reply[reply.find(pwm_to_get)+len(pwm_to_get)+3:-1]
+
+    return status, reply
+
   def get_pos(self):  
     status,pos = self.execute_command("position")
-    
-    return status, pos
+
+    if(not pos==None):
+      self.pos = pos
+    return status, self.pos
+
+  def stop(self):
+    complement = " "+Terminator
+    status,reply = self.execute_command("stop", complement)
+
+    return status, reply
   
   def move(self,pos_0, pos_1):
     complement = " "+(str)(pos_0)+","+(str)(pos_1)+Terminator
@@ -100,17 +149,23 @@ class Finger():
 
     return status, reply
 
-
-    
   def close_port(self):  
     self._ser.close()
   
   def execute_cb(self, goal):
     
-    position_distal = goal.trajectory.points[-1].positions[0]
-    position_proximal = goal.trajectory.points[-1].positions[1]
-    
-    status, reply = self.move(position_distal, position_proximal)
+    position_proximal = goal.trajectory.points[-1].positions[0]
+    position_distal = goal.trajectory.points[-1].positions[1]
+
+    position_proximal = math.degrees(position_proximal)*100 # from radians to centidegrees
+    position_distal = math.degrees(position_distal)*100
+    rospy.logwarn("before")
+    status, reply = self.move(position_proximal, position_distal)
+    rospy.logwarn("after")
+
+    rospy.sleep(1)
+    rospy.logwarn("after sleep")
+    self.stop()
 
     if(status==True):
       self._as.set_succeeded()
@@ -119,11 +174,11 @@ class Finger():
     
   def publish_joint_states(self):
     status, pos = self.get_pos()
-    if status == True:
+    if status == True and (not pos==None):
       try:
-        actual_pos_distal = pos[0]
-        actual_pos_proximal = pos[1]
-        self._joint_states.position = [(int)(actual_pos_distal), (int)(actual_pos_proximal)]
+        actual_pos_proximal = math.radians((float)(pos[0])/100)
+        actual_pos_distal = math.radians((float)(pos[1])/100)
+        self._joint_states.position = [actual_pos_proximal, actual_pos_distal]
         self._joint_states.header.stamp = rospy.Time.now()
         self._pub_joint_states.publish(self._joint_states)
       except ValueError:
@@ -136,37 +191,58 @@ class Finger():
     self._controller_state.actual.velocities = self._joint_states.velocity 
     self._pub_controller_state.publish(self._controller_state)
 
-  def execute_command(self, command, complement=Terminator, timeout=2):
-  
+  def execute_command(self, command, complement=Terminator, timeout=1.2):
+
     t_out = time.time() + timeout
-    
+
+    status,result = False,None
     try:
-    
       self._ser.write(commands[command]+complement)
       line_to_parse = self._ser.readline()
+
       if(command == "position"):
-      
+
         while True:
           if (time.time() > t_out):
-            return False,None
+            break
           elif("P=" not in line_to_parse[0:2]):
             self._ser.write(commands[command]+complement)
             line_to_parse = self._ser.readline()
           else:
             break
-        
+
         line_to_parse = line_to_parse.replace('P=','')
         line_to_parse = line_to_parse.strip()
         line_to_parse = line_to_parse.split(',')
         result = line_to_parse
-        
+
         if(len(result) !=2):
           status = False
         else:
           status=True
-              
+
+      elif(command=="stop"):
+        rospy.loginfo("Stopping the movement")
+        while True:
+          if (time.time() > t_out):
+            break
+          elif("@Sto" not in line_to_parse):
+            self._ser.write(commands[command]+complement)
+            line_to_parse = self._ser.readline()
+          else:
+            rospy.loginfo("Movement stopped")
+            result = True
+            break
+
+      elif(command=="set_pwm"):
+        rospy.loginfo("Setting the PWM: "+complement)
+
+      elif(command=="get_pwm"):
+        result = line_to_parse
+
+
       elif(command == "move"):
-      
+
         while True:
           if (time.time() > t_out):
             return False,None
@@ -175,28 +251,35 @@ class Finger():
             line_to_parse = self._ser.readline()
           else:
             break
-       
+
         result = line_to_parse
-      
+
         status = True
-      
+
       self._ser.flush()
-      
-      return status, result
-      
     except serial.serialutil.SerialException:
       pass
-    
-    
-    
+    except OSError:
+      pass
+    except TypeError:
+      pass
+    return status, result
+
     
 if __name__ == '__main__':
-  rospy.init_node('schunk_finger')
-  fing = Finger("/dev/ttyACM0")
-  r = rospy.Rate(30) # 10hz
-  while not rospy.is_shutdown():
-    fing.publish_joint_states()
-    fing.publish_controller_state()
-    r.sleep()
-  fing.close_port()
+  try:
+    rospy.init_node('schunk_finger')
+    fing = Finger("/dev/ttyACM0")
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+      fing.publish_joint_states()
+      fing.publish_controller_state()
+      r.sleep()
+    fing.close_port()
+  except serial.serialutil.SerialException:
+    pass
+  except OSError:
+    pass
+  except TypeError:
+    pass
 
